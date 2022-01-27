da870ebe5470067b5afddee8e231d66050a651a7
diikshaaaa <diksha.grover@crownstack.com>
2022-01-27 19:34:42 +0530

    made accordion

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Strict mode changes previously accepted "bad syntax" into real errors
